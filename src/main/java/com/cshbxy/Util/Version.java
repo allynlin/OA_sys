c52fe9e5720c38f868261ef8f6cf7d322ce67225
allynlin <allynlin@outlook.com>
2022-11-06 10:55:14 +0800

    git
    Signed-off-by: allynlin <allynlin@outlook.com>

--- src/main/java/com/cshbxy/Util/Version.java
+++ src/main/java/com/cshbxy/Util/Version.java
@@ -1,7 +1,7 @@
 package com.cshbxy.Util;
 
 public class Version {
-    public static String version = "0.9.1";
+    public static String version = "0.9.2";
     public static String lowVersion = "0.9.0";
 
     public static String getVersion() {
