17bd5b4d2acd32188c7b918c837a15a69b3d58d5
allynlin <allynlin@outlook.com>
2022-12-24 15:15:24 +0800

    允许最大文件上传 500 M
    Signed-off-by: allynlin <allynlin@outlook.com>

--- src/main/java/com/cshbxy/Util/Version.java
+++ src/main/java/com/cshbxy/Util/Version.java
@@ -1,7 +1,7 @@
 package com.cshbxy.Util;
 
 public class Version {
-    public static String version = "3.0.0";
+    public static String version = "3.0.1";
     public static String lowVersion = "3.0.0";
 
     public static String getVersion() {
